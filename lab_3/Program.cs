f7c5038bbf8aed1445306a5b763d74a6b5fe5d4f
milan-murray <milan-murray@outlook.ie>
2022-10-05 20:38:40 +0100

    Lab 3 progress

--- lab_3/Program.cs
+++ lab_3/Program.cs
@@ -18,17 +18,49 @@ IHasVolume interface
     1. The interface should have a method which calculates the volume of an item 
 */
 
-interface IHasVolume
+public interface IHasVolume
 {
-	int volume();
+	public double calc_volume();
 }
 
+/*
+Sphere class
+    1. The Sphere class should implement the HasVolume interface.
+    2. The class should have field and matching read-write property for the radius of the sphere.
+    3. Implement appropriate constructors for the class.
+    4. Implement appropriate properties for the class.
+    5. Implement a method to calculate the volume of the sphere i.e. PI * radius * radius * radius. This method is required since the class implements the IHasVolume interface.
+    6. Override the ToString() method inherited from Object, make it display information about the sphere.
+    7. Test the class. Call the method to calculate the volume of a sphere polymorphically via a reference of type IHasVolume. Display details of the sphere object. 
+    8. Make a collection of spheres and call various methods.
+*/
+
+public class Sphere : IHasVolume
+{
+	public double radius { get; set; }
+
+	public double calc_volume()
+	{
+		return 3/4 * Math.PI * Math.Pow(radius, 3);
+	}
+
+	public override string ToString()
+	{
+		return "Una esfera con radio de " + radius;
+	}
+
+	public Sphere(double radius_in)
+	{
+		radius = radius_in;
+	}
+}
 
 public class Test
 {
 	public static void Main()
 	{
-		Console.WriteLine("Test class");
+		Sphere jim = new(radius_in: 22.2);
+		Console.WriteLine(jim);
 	}
 
 }
